0b6f24f40bde5db39272f411b254eb2256cc5b51
SulimanArmoush <suliman221232@hotmail.com.com>
2024-04-27 20:19:27 +0300

    AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,9 +9,12 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule as ValidationRule;
+use App\Traits\PhotoTrait;
 
 class AuthController extends Controller
 {
+
+    use PhotoTrait;
 
     public function register(Request $request)
     {
@@ -46,37 +49,6 @@ class AuthController extends Controller
         return response()->json(['data'=>$data, 'status' => 200, 'message' => 'signed up successfully']);
     }
 
-    public function profile(Request $request){
-
-        $validator = validator::make($request->all(), [
-        'phone' => ['string', 'max:10', 'min:10', 'regex:/^09[0-9]{8}/', ValidationRule::unique(table: 'users')], //syrian number
-        'age' => ['integer'],
-        'address'=> ['string','max:255'],
-        'img' => 'array|size:2', 
-        'img.*' => 'image|mimes:jpeg,png,jpg,gif|max:2048', 
-        ]);
-        if ($validator->fails()) {
-            return response()->json($validator->errors()->all(), status: 400);
-        }
-
-        $imgs = $request->file('img');
-        $imgPaths = [];
-        foreach ($imgs as $index => $img) {
-        $imgPath = $img->store('images', 'public');
-        $imgPaths[] = $imgPath;
-        }
-
-        User::find(Auth::id())->update([
-            'phone'=> $request->phone,
-            'address'=> $request->address,
-            'age'=> $request->age,
-            'photo' => $imgPaths[0],
-            'passport' => $imgPaths[1],
-        ]) ;
-        return response()->json(['message' => 'Your profile updated successfully']);
-    }
-
-
     public function login(Request $request)
     {
         $validator = validator::make($request->all(), [
@@ -107,13 +79,67 @@ class AuthController extends Controller
         return response()->json(['data'=> $data, 'status' => 200, 'message' => 'logedd In successfully']);
     }
 
-
     public function logout(Request $request)
     {
         $request->user()->token()->revoke();
 
         return response()->json(['message' => 'logged out ', 'status' => 200]);
     }
+
+    public function profile(Request $request){
+
+        $validator = validator::make($request->all(), [
+        'phone' => ['string', 'max:10', 'min:10', 'regex:/^09[0-9]{8}/', ValidationRule::unique(table: 'users')], //syrian number
+        'age' => ['integer'],
+        'address'=> ['string','max:255'], 
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->all(), status: 400);
+        }
+        User::find(Auth::id())->update([
+            'phone'=> $request->phone,
+            'address'=> $request->address,
+            'age'=> $request->age,
+        ]) ;
+        return response()->json(['message' => 'Your profile updated successfully']);
+    }
+
+    public function photo(Request $request){
+
+        $validator = validator::make($request->all(), [
+        'photo' => ['image','mimes:jpeg,png,jpg,gif','max:2048'], 
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->all(), status: 400);
+        }
+
+        $photoPath = $this->saveImage($request->photo);
+
+        User::find(Auth::id())->update([
+            'photo' => $photoPath,
+        ]) ;
+
+        return response()->json(['photoPath'=>$photoPath,'message' => 'Your photo added successfully']);
+    }
+
+    public function passport(Request $request){
+
+        $validator = validator::make($request->all(), [
+        'passport' => ['image','mimes:jpeg,png,jpg,gif','max:2048'], 
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->all(), status: 400);
+        }
+
+        $passportPath = $this->saveImage($request->passport);
+
+        User::find(Auth::id())->update([
+            'passport' => $passportPath,
+        ]) ;
+
+        return response()->json(['passportPath'=>$passportPath,'message' => 'Your passport added successfully']);
+    }
+    
 }
 
 
--- routes/api.php
+++ routes/api.php
@@ -8,5 +8,6 @@ Route::controller(AuthController::class)->group(function () {
     Route::Post('register', 'register')->name('register');
     Route::Post('login', 'login')->name('login');
     Route::Post('profile', 'profile')->name('profile')->middleware(['auth:api']);
-
+    Route::Post('photo', 'photo')->name('photo')->middleware(['auth:api']);
+    Route::Post('passport', 'passport')->name('passport')->middleware(['auth:api']);
     });
