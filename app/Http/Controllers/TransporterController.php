034e038878a82ca019416f9a16e370cdcca51d28
SulimanArmoush <suliman221232@hotmail.com.com>
2024-05-07 17:33:35 +0300

    Add func to hotel & restaurant

--- app/Http/Controllers/TransporterController.php
+++ app/Http/Controllers/TransporterController.php
@@ -5,16 +5,11 @@ namespace App\Http\Controllers;
 use App\Models\TheWorld\Facilities\Transporters\Transportation;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use App\Traits\facilityCreateTrait;
-use App\Traits\PhotoTrait;
 use Illuminate\Validation\Rule;
 
 class TransporterController extends Controller
 {
-
-    use facilityCreateTrait, PhotoTrait;
-
-    public function createTransportation(Request $request)
+    public function createTransportations(Request $request)
     {
         $transporter = auth()->user()->facility->transporter;
         $types = $transporter->type == 'air' ? Transportation::$airTypes : Transportation::$landTypes;
@@ -40,18 +35,24 @@ class TransporterController extends Controller
         return response()->json(['message' => 'Your Transportation created successfully'], 200);
     }
 
-    public function getTransportation($transporter_id)
+    public function getTransportation($transportation_id)
+    {
+        $transportation = Transportation::find($transportation_id);
+        return response()->json(['transportation' => $transportation], 200);
+    }
+
+    public function getTransportations($transporter_id)
     {
         $transportations = Transportation::Where('transporter_id', '=', $transporter_id)
             ->paginate(10);
         return response()->json(['transportations' => $transportations], 200);
     }
 
-    public function getAvailableTransportation($transporter_id)
+    public function getAvailableTransportations($transporter_id)
     {
         $transportations = Transportation::
-              Where('transporter_id', $transporter_id)
-            ->where('status','available')
+        Where('transporter_id', $transporter_id)
+            ->where('status', 'available')
             ->paginate(10);
         return response()->json(['transportations' => $transportations], 200);
     }
