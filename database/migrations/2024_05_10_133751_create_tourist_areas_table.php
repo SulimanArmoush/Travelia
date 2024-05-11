7c98261ca7f2cd11805a0f58f1ad209941192d8c
SulimanArmoush <suliman221232@hotmail.com.com>
2024-05-11 16:47:08 +0300

    Edit

--- database/migrations/2024_05_10_133751_create_tourist_areas_table.php
+++ database/migrations/2024_05_10_133751_create_tourist_areas_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('touristAreas', function (Blueprint $table) {
+        Schema::create('tourist_areas', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('description');
